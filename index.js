9707378793ce78fc90f6224fa49067d790ba9cfb
Ahmet Fabian Kourt Neyra Gomez <raztenia@gmail.com>
2021-09-18 22:11:45 -0500

    terminadas todas las funcionalidades CRUD

--- index.js
+++ index.js
@@ -1,34 +1,105 @@
-//obtener elementos de HTML
+//obtener elementos de HTML por ID
 var inputObjeto = document.getElementById('textObjeto');
+var inputElemento = document.getElementById('inputElementoColeccionable');
 var botonGuardarObjeto = document.getElementById('guardarObjeto');
+var botonAgregarElemento = document.getElementById('guardarEnColeccion');
 var botonReiniciar = document.getElementById('reiniciar');
-var H1titulo = document.getElementById('titulo');
+var Header = document.getElementById('titulo');
 var botonOcultarInstructivo = document.getElementById('ocultarInstructivo');
 var seccionInstructivo = document.getElementById('instructivo');
 var seccionInstrucciones = document.getElementById('instrucciones');
+var listaDeColeccion = document.getElementById('listaColeccion');
+var elementos = []
+
+
 
 //Sección para definir qué elementos se van a mostrar al cargar la página
-window.onload = cargarColeccion();
-function cargarColeccion(){
+window.onload = () => {
+    const elementosColeccionables = JSON.parse(window.localStorage.getItem('elementos') || "[]");
+    elementos = elementosColeccionables;
+    elementos.forEach(elemento => {
+        agregaralHTML(elemento);
+    });
     if(window.localStorage.getItem('objeto') !== null){
-        H1titulo.innerText = 'Mi colección de ' + window.localStorage.getItem('objeto');
+        Header.innerText = 'Mi colección de ' + window.localStorage.getItem('objeto');
     }
     if(window.localStorage.getItem('ocultarInstructivo') !== null){
         var borrado = seccionInstrucciones.parentNode.removeChild(seccionInstrucciones);
     }
 }
+//funcion para agregar elementos a la lista
+function agregarElemento(nuevoElemento){
+        const elementoObjeto = {id:Math.random() ,...nuevoElemento};
+        elementos.push(elementoObjeto);
+        window.localStorage.setItem('elementos', JSON.stringify(elementos));
+        return elementos;
+}
+botonAgregarElemento.addEventListener('click',() => {
+    const valorInputElemento = inputElemento.value;
+    console.log('valorInputElemento ',valorInputElemento)
+    const nuevoElemento = { name: valorInputElemento};
+    agregarElemento(nuevoElemento);
+    location.reload();
+})
 
-function guardarElemento(){
-
+//funcion y boton para eliminar elemento
+function eliminarElemento(idelemento){
+    const listafiltrada = elementos.filter((elemento) =>{
+        return elemento.id != idelemento
+    })
+    elementos = listafiltrada
+    window.localStorage.setItem('elementos', JSON.stringify(elementos));
+    location.reload();
 }
 
-function eliminarElemento(){
 
+function editarElemento(idelemento){
+    console.log(idelemento)
+    agregarAlDomParaEdit(idelemento)
 }
-
-function editarElemento(){
-
+//funcion para agregar los elementos de la colección en el DOM
+function agregaralHTML(elemento){
+    const li = document.createElement('li')
+    li.setAttribute('id','li' + elemento.id)
+    li.setAttribute('class','lista')
+    const botonEdit = document.createElement('button')
+    botonEdit.addEventListener('click',() => {
+        editarElemento(elemento.id)
+        
+    })
+    const botonElim = document.createElement('button')
+    botonElim.addEventListener('click',() => {
+        eliminarElemento(elemento.id)
+    })
+    const p = document.createElement('p')
+    p.textContent = elemento.name;
+    botonEdit.innerHTML = 'Editar'
+    botonElim.innerHTML = 'Eliminar'
+    li.appendChild(p)
+    li.appendChild(botonElim)
+    li.appendChild(botonEdit)
+    listaDeColeccion.appendChild(li)
 }
+function agregarAlDomParaEdit(idelemento){
+    const lugarDeLaLista = document.getElementById('li' + idelemento)
+    const inputEdit = document.createElement('input')
+    const botonsave = document.createElement('button')
+    botonsave.addEventListener('click',() => {
+        eliminarElemento(idelemento)
+        agregarElemento({ name: inputEdit.value});
+    })
+    botonsave.innerHTML = 'Actualizar'
+    const botonCancelar = document.createElement('button')
+    botonCancelar.addEventListener('click',() => {
+        location.reload();
+    })
+    botonCancelar.innerHTML = 'Cancelar'
+    const ul = document.createElement('ul')
+    ul.appendChild(inputEdit)
+    ul.appendChild(botonsave)
+    ul.appendChild(botonCancelar)
+    lugarDeLaLista.appendChild(ul)
+} 
 //boton para ocultar instructivo
 botonOcultarInstructivo.addEventListener('click',() => {
     const ocultarInstructivo = 1;
